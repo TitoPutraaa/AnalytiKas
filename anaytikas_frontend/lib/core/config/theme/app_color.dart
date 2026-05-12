d8177487790a5f9baf0d54a397b1da8ef3dad494
TitoPutraaa <titobamulo13@gmail.com>
2026-05-12 13:05:06 +0800

    struktur folder dan sedikit theme

---
+++ anaytikas_frontend/lib/core/config/theme/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/animation.dart';
+
+class AppColor {
+  static final primary = Color(0xff1A3263);
+  static final black = Color(0xff000000);
+  static final white = Color(0xffffffff);
+  static final gray = Color(0xff808080);
+}
