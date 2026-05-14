2962db3d9171f6364ce6903859d688aa7589a33d
TitoPutraaa <titobamulo13@gmail.com>
2026-05-14 23:45:47 +0800

    add produk card

--- anaytikas_frontend/lib/core/config/theme/app_theme.dart
+++ anaytikas_frontend/lib/core/config/theme/app_theme.dart
@@ -3,33 +3,33 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static final mainTheme = ThemeData(
-    primaryColor: AppColor.black,
-    scaffoldBackgroundColor: AppColor.white,
+    useMaterial3: true,
+    fontFamily: 'Rubik',
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColor.primary,
+      brightness: Brightness.light,
+    ),
+    appBarTheme: AppBarThemeData(backgroundColor: Colors.red),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColor.primary,
         foregroundColor: Colors.white,
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       ),
     ),
-    iconTheme: const IconThemeData(color: Color(0xFF1A237E), fill: 1.0),
-  );
 
-  static final textTheme = TextTheme(
-    // bisa di sesuain aja gais
-    displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-    headlineLarge: TextStyle(
-      fontSize: 20,
-      fontWeight: FontWeight.bold,
-    ), // buat header fiutre ex: kasir, stok
-    labelLarge: TextStyle(
-      fontSize: 18,
-      fontWeight: FontWeight.w500,
-    ), // buat nama barang
-    labelSmall: TextStyle(
-      fontSize: 14,
-      fontWeight: FontWeight.w300,
-    ), // deskripsi
+    textTheme: TextTheme(
+      displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      headlineLarge: TextStyle(
+        // buat nama produk
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+      ),
+      labelLarge: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w300,
+      ), // buat nama barang
+      labelSmall: TextStyle(fontSize: 8, fontWeight: FontWeight.w200),
+    ),
   );
 }
--- anaytikas_frontend/lib/features/stok/presentation/pages/homeStok.dart
+++ anaytikas_frontend/lib/features/stok/presentation/pages/homeStok.dart
@@ -1,3 +1,5 @@
+import 'package:anaytikas_frontend/core/config/theme/app_color.dart';
+import 'package:anaytikas_frontend/features/kasir/presentation/widgets/produkStok.dart';
 import 'package:flutter/material.dart';
 
 class Homestok extends StatelessWidget {
@@ -5,6 +7,44 @@ class Homestok extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Home Stok", style: TextStyle(fontSize: 40));
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        spacing: 10,
+        children: [
+          Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add),
+                      label: const Text("Barang Baru"),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add),
+                      label: const Text("Operasional"),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Expanded(child: ProdukStok()),
+        ],
+      ),
+    );
   }
 }
