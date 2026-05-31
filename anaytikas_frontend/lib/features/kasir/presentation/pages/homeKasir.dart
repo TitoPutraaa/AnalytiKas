6f55f6b99d0e26fdb4412a9507253542be6276f2
mwidiarta <madewidi185@gmail.com>
2026-06-01 03:26:00 +0800

    finish HomeKasirUI v1

--- anaytikas_frontend/lib/features/kasir/presentation/pages/homeKasir.dart
+++ anaytikas_frontend/lib/features/kasir/presentation/pages/homeKasir.dart
@@ -1,150 +1,132 @@
+import '../../../../core/config/theme/app_color.dart';
+import '../widgets/product_card_item.dart';
 import 'package:flutter/material.dart';
 
-class Homekasir extends StatelessWidget {
+class Homekasir extends StatefulWidget {
   const Homekasir({super.key});
 
   @override
+  State<Homekasir> createState() => _HomekasirState();
+}
+
+class _HomekasirState extends State<Homekasir> {
+  String? _selectedFilter;
+  final List<String> categories = [
+    'Alat Mandi',
+    'Snack',
+    'Alat Tulis',
+    'Minuman',
+    'Mie Instant',
+  ];
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(left: 16, right: 16),
-      child: Column(
-        children: [
-          // Bagian Search Bar
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            // Search Bar
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Search
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: AppColor.lowGray),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: AppColor.primary),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      prefixIcon: const Icon(Icons.search, size: 20),
+                      hintText: 'Cari nama produk atau SKU...',
+                      hintStyle: const TextStyle(
+                        fontSize: 14,
+                        color: AppColor.lowGray,
+                      ),
                     ),
-                    prefixIcon: const Icon(Icons.search),
-                    hintText: 'Cari nama produk atau SKU...',
                   ),
                 ),
-              ),
-              const SizedBox(width: 20),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.document_scanner_outlined),
-              ),
-            ],
-          ),
-          const SizedBox(height: 16),
-
-          Expanded(
-            child: ListView.builder(
-              itemCount: 5 + 1,
-              itemBuilder: (context, index) {
-                if (index == 0) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 12),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          color: Colors.grey,
-                          border: Border.all(
-                            color: Colors.grey.shade400,
-                            width: 1,
-                          ),
-                        ),
-                        child: DropdownButtonHideUnderline(
-                          child: DropdownButton<String>(
-                            hint: const Text(
-                              'Pilih Kategori',
-                              style: TextStyle(fontSize: 14),
-                            ),
-                            items: const [
-                              DropdownMenuItem(
-                                value: '1',
-                                child: Text(
-                                  'Kopi',
-                                  style: TextStyle(fontSize: 14),
-                                ),
-                              ),
-                              DropdownMenuItem(
-                                value: '2',
-                                child: Text(
-                                  'Teh',
-                                  style: TextStyle(fontSize: 14),
-                                ),
-                              ),
-                            ],
-                            onChanged: (value) {},
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                    ],
-                  );
-                }
-                return Container(
-                  margin: const EdgeInsets.only(bottom: 12),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 16,
+                const SizedBox(width: 10),
+                // Scan
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(color: AppColor.lowGray),
+                    padding: EdgeInsets.zero,
+                    fixedSize: const Size(10, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
                   ),
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 1, color: Colors.grey.shade300),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            Text(
-                              'Kopi Arabika Premium Flores Gayo',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              ),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            SizedBox(height: 4),
-                            Text(
-                              'KODE: KOP-0011212',
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                            SizedBox(height: 8),
-                            Text('Harga: Rp. 10.000'),
-                          ],
-                        ),
-                      ),
-
-                      const SizedBox(width: 12),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          const Text(
-                            'Stok ada',
-                            style: TextStyle(
-                              color: Colors.green,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 16),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: const Icon(Icons.shopping_cart),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              },
+                  child: const Icon(Icons.document_scanner_rounded, size: 25),
+                  onPressed: () {},
+                ),
+              ],
             ),
+            const SizedBox(height: 16),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: 6 + 1,
+                itemBuilder: (context, index) {
+                  // Filter
+                  if (index == 0) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        DropdownMenu<String>(
+                          hintText: 'Pilih Kategori',
+                          onSelected: (String? value) {
+                            setState(() {
+                              _selectedFilter = value;
+                            });
+                          },
+                          dropdownMenuEntries: [
+                            const DropdownMenuEntry(value: 'all', label: 'All'),
+                            ...categories.map(
+                              (item) =>
+                                  DropdownMenuEntry(value: item, label: item),
+                            ),
+                          ],
+                          textStyle: TextStyle(fontSize: 14),
+                          inputDecorationTheme: InputDecorationTheme(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                      ],
+                    );
+                  }
+                  // CardList
+                  return ProductCardItem();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.shopping_cart_rounded, color: Colors.white),
+        label: Text(
+          '3 Produk',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w400,
+            fontSize: 14,
           ),
-        ],
+        ),
+        extendedPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        elevation: 1,
+        backgroundColor: AppColor.primary,
+
+        onPressed: () {},
       ),
     );
   }
