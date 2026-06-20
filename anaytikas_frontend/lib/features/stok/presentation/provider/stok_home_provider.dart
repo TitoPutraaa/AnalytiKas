59ce03c5fde6ebd92bf4f9d399718164730a5346
TitoPutraaa <titobamulo13@gmail.com>
2026-06-20 21:32:08 +0800

    add provider to main

--- anaytikas_frontend/lib/features/stok/presentation/provider/stok_home_provider.dart
+++ anaytikas_frontend/lib/features/stok/presentation/provider/stok_home_provider.dart
@@ -1,3 +1,4 @@
+import 'package:anaytikas_frontend/features/stok/domain/entities/kategori.dart';
 import 'package:anaytikas_frontend/features/stok/domain/entities/product_entity.dart';
 import 'package:anaytikas_frontend/features/stok/domain/usecases/get_all_products.dart';
 import 'package:flutter/material.dart';
@@ -9,14 +10,23 @@ class StokHomeProvider with ChangeNotifier {
   StokHomeProvider({required this.getAllProduct});
 
   Status status = Status.initial;
-  List<ProductEntity> allProduct = [];
+  List<ProductEntity> _allProduct = [];
+  List<ProductEntity> _filteredProduct = [];
+  List<Kategori> _allCategory = [];
+  bool _isLoading = false;
+  bool _isSearching = false;
   String message = "";
+
+  List<ProductEntity> get allProducts => _filteredProduct;
+  List<Kategori> get allCategory => _allCategory;
+  bool get isLoading => _isLoading;
+  bool get isSearching => _isSearching;
 
   Future<void> getAllProducts() async {
     status = Status.loading;
     notifyListeners();
     try {
-      allProduct = await getAllProduct.call();
+      _allProduct = await getAllProduct.call();
       status = Status.success;
       notifyListeners();
     } catch (e) {
@@ -24,6 +34,29 @@ class StokHomeProvider with ChangeNotifier {
       message = "gagal memuat semua product provider. err: ${e.toString()}";
       notifyListeners();
     }
+    notifyListeners();
+  }
+
+  void filterProducts({String query = "", int? categoryId}) {
+    final isFiltering =
+        query.isNotEmpty || (categoryId != null && categoryId != 0);
+    _isSearching = isFiltering;
+
+    _filteredProduct = _allProduct.where((p) {
+      final idProduct = p.idProduct;
+
+      final matchesQuery = p.namaProduct.toLowerCase().contains(
+        query.toLowerCase(),
+      );
+
+      final matchesQueryId = idProduct.contains(query.trim());
+
+      final matchesCategory =
+          (categoryId == null || categoryId == 0) ||
+          p.kategori.idKategori == categoryId;
+      return (matchesQuery || matchesQueryId) && matchesCategory;
+    }).toList();
+
     notifyListeners();
   }
 }
--- anaytikas_frontend/lib/main.dart
+++ anaytikas_frontend/lib/main.dart
@@ -5,6 +5,10 @@ import 'package:anaytikas_frontend/features/kasir/data/repositories/kasir_reposi
 import 'package:anaytikas_frontend/features/kasir/domain/usecases/get_all_product_usecase.dart';
 import 'package:anaytikas_frontend/features/kasir/domain/usecases/save_transaction_usecase.dart';
 import 'package:anaytikas_frontend/features/kasir/presentation/manager/kasir_provider.dart';
+import 'package:anaytikas_frontend/features/stok/data/repository/stok_repository_impl.dart';
+import 'package:anaytikas_frontend/features/stok/data/sources/stok_local_datasource.dart';
+import 'package:anaytikas_frontend/features/stok/domain/usecases/get_all_products.dart';
+import 'package:anaytikas_frontend/features/stok/presentation/provider/stok_home_provider.dart';
 import 'package:anaytikas_frontend/shared/widgets/main_sheel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,6 +19,9 @@ void main() {
   final kasirRepo = KasirRepositoryImpl(
     localDataSource: KasirLocalDataSourceImpl(dbHelper: db),
   );
+  final stokRepo = StokRepositoryImpl(
+    datasource: StokLocalDatasourceImpl(dbHelper: db),
+  );
   // final
   runApp(
     MultiProvider(
@@ -24,6 +31,11 @@ void main() {
             getAllProduct: GetAllProductUsecase(kasirRepo),
             saveTransaction: SaveTransactionUsecase(kasirRepo),
           ),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => StokHomeProvider(
+            getAllProduct: GetAllProducts(stokRepository: stokRepo),
+          )..getAllProducts(),
         ),
       ],
       child: MyApp(),
