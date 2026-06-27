ce8eafdf0329c694010c84a33d9d75299588ed3f
TitoPutraaa <titobamulo13@gmail.com>
2026-06-28 04:18:15 +0800

    add search filter in stok

--- anaytikas_frontend/lib/features/stok/presentation/pages/home_stok.dart
+++ anaytikas_frontend/lib/features/stok/presentation/pages/home_stok.dart
@@ -2,12 +2,20 @@ import 'package:anaytikas_frontend/core/config/theme/app_color.dart';
 import 'package:anaytikas_frontend/features/stok/presentation/pages/barang_baru.dart';
 import 'package:anaytikas_frontend/features/stok/presentation/pages/ops_stok.dart';
 import 'package:anaytikas_frontend/features/stok/presentation/provider/stok_home_provider.dart';
+import 'package:anaytikas_frontend/features/stok/presentation/widgets/camera_scan.dart';
 import 'package:anaytikas_frontend/features/stok/presentation/widgets/produk_stok_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class Homestok extends StatelessWidget {
-  const Homestok({super.key});
+class HomeStok extends StatefulWidget {
+  const HomeStok({super.key});
+
+  @override
+  State<HomeStok> createState() => _HomeStokState();
+}
+
+class _HomeStokState extends State<HomeStok> {
+  final _kodeBarangController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -71,26 +79,56 @@ class Homestok extends StatelessWidget {
             ],
           ),
 
-          TextField(
-            onChanged: (value) {
-              context.read<StokHomeProvider>().searchProduct(value);
-            },
-            decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: AppColor.lowGray),
-                borderRadius: BorderRadius.circular(8),
+          Row(
+            children: [
+              TextField(
+                controller: _kodeBarangController,
+                onChanged: (value) {
+                  context.read<StokHomeProvider>().searchProduct(value);
+                },
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: AppColor.lowGray),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: AppColor.primary),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  prefixIcon: const Icon(Icons.search, size: 20),
+                  hintText: 'Cari nama produk...',
+                  hintStyle: const TextStyle(
+                    fontSize: 14,
+                    color: AppColor.lowGray,
+                  ),
+                ),
+                onTapOutside: (PointerDownEvent click) {
+                  FocusManager.instance.primaryFocus?.unfocus();
+                },
               ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: AppColor.primary),
-                borderRadius: BorderRadius.circular(8),
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  side: const BorderSide(color: AppColor.black),
+                  padding: EdgeInsets.zero,
+                  fixedSize: const Size(10, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Icon(Icons.document_scanner_rounded, size: 25),
+                onPressed: () async {
+                  final scannedValue = await Navigator.of(context).push<String>(
+                    MaterialPageRoute(builder: (_) => const CameraScan()),
+                  );
+
+                  if (scannedValue != null && mounted) {
+                    setState(() {
+                      _kodeBarangController.text = scannedValue;
+                    });
+                  }
+                },
               ),
-              prefixIcon: const Icon(Icons.search, size: 20),
-              hintText: 'Cari nama produk...',
-              hintStyle: const TextStyle(fontSize: 14, color: AppColor.lowGray),
-            ),
-            onTapOutside: (PointerDownEvent click) {
-              FocusManager.instance.primaryFocus?.unfocus();
-            },
+            ],
           ),
 
           Consumer<StokHomeProvider>(
